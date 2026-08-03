eb5891def8f5616081b72d923b94ecbf78125cd7
DunnTake <moththang2808@gmail.com>
2026-08-04 03:39:41 +0700

    improved prime check

--- C/SimplePrimeCheck.c
+++ C/SimplePrimeCheck.c
@@ -1,27 +1,47 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdint.h>
+#include <time.h>
+
+int elapsed(clock_t t0)
+{
+    clock_t t1 = clock();
+    double delta = (double)(t1 - t0) / CLOCKS_PER_SEC;
+    printf("Runtime: %f seconds\n", delta);
+}
 
 int main()
 {
-    int a;
+
+    int64_t a;
     printf("Enter an integer to test prime: ");
-    scanf("%i",&a);
-    int lim = (int)sqrt(a);
+    scanf("%lld",&a);
+    if (a < 0) {printf("Invalid, int must be positive"); return 0;};
+    int64_t lim = (int64_t)sqrt(a);
 
     //Default primes
-    if (a == 2 || a == 3 || a == 5) {printf("%i is not prime\n", a); return 0;}
+    if (a == 2 || a == 3 || a == 5) {printf("%lld is not prime\n", a); return 0;}
     //Default not primes
-    if (a % lim == 0 || a % 2 == 0 || a % 3 == 0 || a == 1) {printf("%i is not prime\n", a); return 0;}
+    if (a % 2 == 0 || a % 3 == 0 || a == 1 || a % lim == 0) {printf("%lld is not prime\n", a); return 0;}
     
-    int k = 1;
-    while (1)
-    {
+    clock_t t0 = clock();
+    int64_t k = 1;
+    for (;;) {
         if (6*k-1 > lim) {break;}
-        if (a % (6*k-1) == 0) {printf("%i is not prime\n", a); return 0;}
+        if (a % (6*k-1) == 0) {
+            printf("%lld is not prime\n", a);
+            elapsed(t0);
+            return 0;
+        }
         if (6*k+1 > lim) {break;}
-        if (a % (6*k+1) == 0) {printf("%i is not prime\n", a); return 0;}
+        if (a % (6*k+1) == 0) {
+            printf("%lld is not prime\n", a);
+            elapsed(t0);
+            return 0;
+        }
         k++;
     }
-    printf("%i is prime\n", a);
+    printf("%lld is prime\n", a);
+    elapsed(t0);
     return 0;
 }
